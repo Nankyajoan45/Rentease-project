489894fbb122b960a8b5c6404d7dae963970c2d4
Nankya Joan <nankyajoan45@gmail.com>
2026-05-28 10:17:27 +0300

    editted the DB host name and port

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,10 @@
 <?php
 // Database Configuration
-define('DB_HOST', $_ENV['DB_HOST'] ?? 'zephyr.proxy.rlwy.net');
+define('DB_HOST', $_ENV['DB_HOST'] ?? 'mysql.railway.internal');
 define('DB_USER', $_ENV['DB_USER'] ?? 'root');
 define('DB_PASS', $_ENV['DB_PASS'] ?? 'qCdPtMjWyytMoMGpdELxiYwNGOGVTLHM');
 define('DB_NAME', $_ENV['DB_NAME'] ?? 'railway');
-define('DB_PORT', (int)($_ENV['DB_PORT'] ?? 56540));
+define('DB_PORT', (int)($_ENV['DB_PORT'] ?? 3306));
 
 // App Config
 define('APP_NAME', $_ENV['APP_NAME'] ?? 'RentEase');
